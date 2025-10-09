9f2500a8709dd50cff6d40077815def472ba63fa
Tana' Turay Titiyon <animist_squib0z@icloud.com>
2025-10-09 16:51:15 +0800

    Create expenditure entity

---
+++ BookKeeper/BookKeeper/BookKeeper.Api/Entities/Expenditure.cs
@@ -0,0 +1,11 @@
+﻿namespace BookKeeper.Api.Entities;
+
+public class Expenditure
+{
+    public string Id { get; private set; }
+    public string PaymentName { get; private set; }
+    public decimal Amount { get; private set; }
+    public DateOnly PaymentDateOnUtc { get; private set; }
+    public DateOnly CreatedOnUtc { get; private set; }
+    public DateOnly? UpdatedOnUtc { get; private set; }
+}
