e0369ab0a6774069d43ec60376d82879ba0220ad
Tana' Turay Titiyon <animist_squib0z@icloud.com>
2025-10-13 16:38:50 +0800

    Add Label entity with creation and update methods

--- BookKeeper/BookKeeper/BookKeeper.Api/Entities/Expenditure.cs
+++ BookKeeper/BookKeeper/BookKeeper.Api/Entities/Expenditure.cs
@@ -1,11 +1,28 @@
 ﻿namespace BookKeeper.Api.Entities;
 
-public class Expenditure
+public sealed class Expenditure
 {
+    private Expenditure()
+    {
+    }
+
     public string Id { get; private set; }
     public string PaymentName { get; private set; }
     public decimal Amount { get; private set; }
     public DateOnly PaymentDateOnUtc { get; private set; }
-    public DateOnly CreatedOnUtc { get; private set; }
-    public DateOnly? UpdatedOnUtc { get; private set; }
+    public DateTime CreatedOnUtc { get; private set; }
+    public DateTime? UpdatedOnUtc { get; private set; }
+
+    public static Expenditure Create(
+        string paymentName,
+        decimal amount,
+        DateOnly paymentDateOnUtc) =>
+        new()
+        {
+            Id = $"e_{Ulid.NewUlid()}",
+            PaymentName = paymentName,
+            Amount = amount,
+            PaymentDateOnUtc = paymentDateOnUtc,
+            CreatedOnUtc = DateTime.UtcNow
+        };
 }
---
+++ BookKeeper/BookKeeper/BookKeeper.Api/Entities/Label.cs
@@ -0,0 +1,42 @@
+﻿namespace BookKeeper.Api.Entities;
+
+public sealed class Label
+{
+    private Label()
+    {
+    }
+
+    public string Id { get; private set; }
+    public string Name { get; private set; }
+    public bool IsDeleted { get; private set; }
+    public bool IsIncome { get; private set; }
+    public DateTime CreateOnUtc { get; private set; }
+    public DateTime? UpdatedOnUtc { get; private set; }
+
+    public static Label Create(
+        string name,
+        bool isIncome) =>
+        new()
+        {
+            Id = $"l_{Ulid.NewUlid()}",
+            Name = name,
+            IsIncome = isIncome,
+            IsDeleted = false,
+            CreateOnUtc = DateTime.UtcNow
+        };
+
+    public void Update(
+        string name,
+        bool isIncome)
+    {
+        Name = name;
+        IsIncome = isIncome;
+        UpdatedOnUtc = DateTime.UtcNow;
+    }
+
+    public void Deleted()
+    {
+        IsDeleted = true;
+        UpdatedOnUtc = DateTime.UtcNow;
+    }
+}
