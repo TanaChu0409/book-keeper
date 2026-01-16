90b2dd61697f37ebe16a276e9f328c9155684eba
Tana' Turay Titiyon <109675132+TanaChu0409@users.noreply.github.com>
2026-01-16 16:48:57 +0800

    Add StatisticOfDate entity and initial processing job
    Introduces the StatisticOfDate entity, its EF Core configuration, and a Quartz job stub for processing daily statistics by user. This sets up the foundation for tracking and aggregating daily income and expenditure statistics.

---
+++ BookKeeper/BookKeeper/BookKeeper.Api/Entities/StatisticOfDate.cs
@@ -0,0 +1,30 @@
+﻿namespace BookKeeper.Api.Entities;
+
+public class StatisticOfDate
+{
+    private StatisticOfDate()
+    {
+    }
+
+    public string Id { get; private set; }
+    public DateOnly DateOnUtc { get; private set; }
+    public decimal TotalExpendAmount { get; private set; }
+    public decimal TotalIncomeAmount { get; private set; }
+    public decimal SumAmount { get; private set; }
+    public string UserId { get; private set; }
+
+    public static StatisticOfDate Create(
+        DateOnly dateOnUtc,
+        decimal totalExpendAmount,
+        decimal totalIncomeAmount,
+        string userId) =>
+        new()
+        {
+            Id = $"sod_{Ulid.NewUlid()}",
+            DateOnUtc = dateOnUtc,
+            TotalExpendAmount = totalExpendAmount,
+            TotalIncomeAmount = totalIncomeAmount,
+            SumAmount = totalIncomeAmount - totalExpendAmount,
+            UserId = userId
+        };
+}
---
+++ BookKeeper/BookKeeper/BookKeeper.Api/Features/Statistics/CreateStatisticOfDate.cs
@@ -0,0 +1,33 @@
+﻿using BookKeeper.Api.Database;
+using Quartz;
+
+namespace BookKeeper.Api.Features.Statistics;
+
+public static class CreateStatisticOfDate
+{
+    internal sealed class ProcessStatisticOfDate(
+        ApplicationDbContext applicationDbContext,
+        ILogger<ProcessStatisticOfDate> logger)
+        : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            logger.LogInformation("Begin processing CreateStatisticOfDate");
+
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            // get this date income data and group by user id
+            var incomeByUserIds = applicationDbContext.Incomes
+                .Where(i => i.IncomeDateOnUtc == today)
+                .GroupBy(i => i.UserId)
+                .ToList();
+            // get this date expenditure data and group by user id
+            var expenditureByUserIds = applicationDbContext.Expenditures
+                .Where(e => e.PaymentDateOnUtc == today)
+                .GroupBy(e => e.UserId)
+                .ToList();
+
+            throw new NotImplementedException();
+        }
+    }
+}
